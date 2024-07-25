15a315dbbca559e2a7ca445a3028f8f93adad078
chaitu <chaypokuri@gmail.com>
2024-07-25 20:51:36 +0530

    changes are modified

--- azmain.tf
+++ azmain.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
   features {}
 }
+subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
+  client_id       = "c2bd123a-183f-43d5-bf41-c725494e595a"
+  client_secret   = "5RL7xPKpQhPo/rbfYYeten5uOZ/Vx6z/rniNTJZU"
+  tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
 resource "azurerm_resource_group" "rg" {
 
   name     = "myResourceGroup"
