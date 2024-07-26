5b08844cca538104d59f4176ad2e5aeb1373edc5
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-07-26 18:06:40 +0530

    Update azmain.tf

--- azmain.tf
+++ azmain.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  SUBSCRIPTION_ID = "c2bd123a-183f-43d5-bf41-c725494e595a"
-    client_id  = "1a046c02-8c39-4f1d-b30b-93f41a9c6b1ii5"
-     client_secret = "66q8Q~jSzkeybn43VuFkxkGz6wpfMHEGPxZaKcsE"
   tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
+  client_id  = "1a046c02-8c39-4f1d-b30b-93f41a9c6b1ii5"
+  client_secret = "66q8Q~jSzkeybn43VuFkxkGz6wpfMHEGPxZaKcsE"
+  SUBSCRIPTION_ID = "c2bd123a-183f-43d5-bf41-c725494e595a"
   }
 resource "azurerm_resource_group" "rg" {
 
