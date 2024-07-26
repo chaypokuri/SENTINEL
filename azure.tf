9463ee7544379092327e7c41b006083c08c8a846
chaitu <chaypokuri@gmail.com>
2024-07-26 18:51:31 +0530

    modified changes

--- azure.tf
+++ azure.tf
@@ -4,12 +4,6 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "example" {
-  name     = example-resources
-  location = East US
+  name     = "example-resources"
+  location = "East US"
 }
-storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
