d97c4443975453c903202aedee64dca88b4b30ea
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-08-07 13:10:15 +0530

    Update azmain.tf

--- azmain.tf
+++ azmain.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+     azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.59.0"
+    } 
+  }
+  required_version = ">= 0.14.9"
+}
 provider "azurerm" {
   features {}
   tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
@@ -5,67 +14,69 @@ provider "azurerm" {
   client_secret = "66q8Q~jSzkeybn43VuFkxkGz6wpfMHEGPxZaKcsE"
   subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
   }
-resource "azurerm_resource_group" "rg" {
+variable "prefix" {
+  default = "terraform"
+}
 
-  name     = "myResourceGroup"
-  location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-ResourceGroup"
+  location = "Central India"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myVnet"
+  name                = "${var.prefix}-VNet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "myNIC"
+  name                = "${var.prefix}-NIC"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name                          = "tfconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "myVM"
+  name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_DS1_v2"
 
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
   storage_os_disk {
-    name              = "myOSDisk"
+    name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
   os_profile {
     computer_name  = "hostname"
-    admin_username = "azureuser"
-    admin_password = "P@ssw0rd1234!"
+    admin_username = "tfadmin"
+    admin_password = "Password1234!"
   }
-
   os_profile_linux_config {
     disable_password_authentication = false
   }
+  tags = {
+    environment = "staging"
+  }
 }
-
